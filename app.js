f7d4f6e91f6e26fedba9b984a1f9c93cca05a841
DrewStroede <dstrow13@gmail.com>
2023-04-15 20:09:21 -0500

    still learning javascript

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let userName = prompt("What is your name?");
-console.log("Hi "+ userName + ", lets get fit!");
-document.write("Hi "+ userName + ", let's get fit!");
+console.log ("Hi "+ userName + ", lets get fit!");
+document.write ("Hi "+ userName + ", let's get fit!");
 
 
 let userAnswer = prompt("How many years of experience do you have in the gym?");
 
 if(userAnswer == 1){
-    alert("We've got a solid start! Lets take it to the next level");
+    alert ("We've got a solid start! Lets take it to the next level");
 } else if(userAnswer < 1 ){
-    alert("You're at the perfect place to get started!");
+    alert ("You're at the perfect place to get started!");
 }else if(userAnswer > 2 ){
-alert("Whoa! You must be a pro! Lets see if I cant teach you a thing or two!");
+alert ("Whoa! You must be a pro! Lets see if I cant teach you a thing or two!");
 }
-else if(userAnswer > 50 ){
-    alert("Were you born in the gym? I'm not sure you are going to learn anything new here...");
+else if (userAnswer > 50 ){
+    alert ("Were you born in the gym? I'm not sure you are going to learn anything new here...");
 }
