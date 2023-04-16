f30a5801f97547f66d8a920f0cab1241949f508f
DrewStroede <dstrow13@gmail.com>
2023-04-15 21:25:50 -0500

    Changes my js questions for fluidity

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-function findName(){
-let userName = prompt("What is your name?");
-console.log ("Hi "+ userName + ", lets get fit!");
-document.write ("Hi "+ userName + ", let's get fit!");
+alert("This content is only for humans...Are you human? (Y or N");{
+    let userGo = prompt("This content is only for humans...Are you human? (Y or N");
+    if(userGo = Y){
+        document.write ("Let's take a look");
+    } else if(userGo != Y){
+        document.write ("Hello, robot. I suppose I can still show you my website.");
+    }
 }
 
-function findExperience(){
+alert("What is your name?"); {
+let userName = prompt("What is your name?");
+console.log(userName);
+document.write ("You ready to get after it "+ userName + "?");
+}
+
+alert("How many years of experience do you have in the gym?"); {
 let userAnswer = prompt("How many years of experience do you have in the gym?");
 if(userAnswer == 1){
     alert ("We've got a solid start! Lets take it to the next level");
@@ -16,13 +25,4 @@ alert ("Whoa! You must be a pro! Lets see if I cant teach you a thing or two!");
 else if (userAnswer > 50 ){
     alert ("Were you born in the gym? I'm not sure you are going to learn anything new here...");
 }
-}
-
-function findEntrance(){
-    let userGo = prompt("This content is only for humans...Are you human? (Y or N");
-    if(userGo = Y){
-        alert ("Let's take a look");
-    } else if(userGo != Y){
-        alert ("Hello, robot. I suppose I can still show you my website.");
-    }
 }
