7b0c1b1286bb1b562c62d26f79a51a0e28670567
Drew Stroede <128856835+Dstroede@users.noreply.github.com>
2023-04-15 21:58:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-let userGo= prompt("This content is only for humans...Are you human? (Y or N");
+
+let(userGo)= prompt("This content is only for humans...Are you human? (Y or N");
  if(userGo=== Y){
  document.write("Let's take a look");
- } else if(userGo == N){
+ } else if(userGo === N){
     document.write("Hello, robot. I suppose I can still show you my website.");
-}
+} console.log (userGo)
 
 
-let userName= prompt("What is your name?");
+let(userName)= prompt("What is your name?");
 console.log(userName);
 document.write("You ready to get after it "+ userName + "?");
 
 
-let userAnswer= prompt("How many yrs of exp do you have in the gym?");
+let(userAnswer)= prompt("How many yrs of exp do you have in the gym?");
 if(userAnswer=== 1){
     alert("We've got a solid start! Lets take it to the next level");
 } else if(userAnswer < 1 ){
@@ -21,4 +22,4 @@ alert ("Whoa! You must be a pro! Lets see if I cant teach you a thing or two!");
 }
 else if(userAnswer > 50 ){
     alert("Were you born in the gym? I'm not sure you are going to learn anything new here...");
-}
+} console.log (userAnswer)
