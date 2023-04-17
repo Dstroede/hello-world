9f10bec4fe3d9b8a29f59929e742c98c546df77b
DrewStroede <dstrow13@gmail.com>
2023-04-17 06:00:59 -0500

    added fuctions for lab 7

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+function allowEntrance(){
 let webPassword = ("Y");
 let userGo = prompt("This is only for humans...Are you human? (Y or N)");
 console.log(userGo)
@@ -6,9 +7,13 @@ console.log(userGo)
  } while(userGo != webPassword){
 userGo= prompt("No robots here! Unless... Are you at least half human?");
 }
+}
+function collectName (){
 let userName = prompt("What is your name?");
 console.log(userName);
 alert("You ready to get after it "+ userName + "?");
+}
+function collectXp (){
 let userAnswer = prompt("How many yrs of exp do you have in the gym?");
 console.log(userAnswer)
 if(userAnswer == "1"){
@@ -20,4 +25,5 @@ alert("Whoa! You must be a pro! Lets see if I cant teach you a thing or two!");
 }
 else if (userAnswer > "50" ){
     alert("Were you born in the gym? I'm not sure you are going to learn anything new here...");
+}
 }
